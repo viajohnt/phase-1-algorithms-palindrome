aef5ac5e6255cb87fb142b7639442277f280509e
John Truong <viajohnt@gmail.com>
2023-03-27 10:35:51 -0600

    Final Commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word.split('').reverse().join('') === word) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
+
+
+
 
 /* 
   Add your pseudocode here
-*/
+// Create a function that returns a string true if the words are able to be read
+the same forwards and backwards.
+If it is not, return false.
 
 /*
   Add written explanation of your solution here
+  The function first spliets the word "r a c e c a r", then it reverses the string,
+  then it checks if the reversed string is equal to the original string.
+  If it is, return true, if it is not, return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +37,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
